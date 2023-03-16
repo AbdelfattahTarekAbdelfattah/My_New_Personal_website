0d757270ebc153b5d61418398788d18cc97dd62e
abdelfattah <abdelfattah@gail.com>
2023-03-16 11:47:05 +0200

    My-Personal-Website

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var typed = new Typed('#span', {
+    strings: ["Front-End", "Back-End", "Full-Stack"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+var typed = new Typed('#span2', {
+    strings: ["Front-End", "Back-End", "Full-Stack"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+var typed = new Typed('#span3', {
+    strings: ["Front-End", "Back-End", "Full-Stack"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+// <i class="fa-solid fa-arrow-up"></i>
+
+if (window.scrollY < 100) {
+    
+}
